83bab4eb3f8c70ef05b896219fe126c4e0d2a7b0
Roy Tay <toyray@users.noreply.github.com>
2014-11-04 15:05:32 +0000

    Formatting changes

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
   describe "GET index" do
-    it "renders template" do
+    it 'renders template' do
       get :index
       expect(response).to render_template :index
     end
