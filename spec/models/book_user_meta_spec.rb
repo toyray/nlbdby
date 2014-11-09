d6ae97dbdf75cc3cf511ce7d27998a3fa08e1904
Roy Tay <toyray@users.noreply.github.com>
2014-11-09 14:22:31 +0000

    Changed old should syntax for specs to the new expect syntax

--- spec/models/book_user_meta_spec.rb
+++ spec/models/book_user_meta_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe BookUserMeta, :type => :model do
   context 'associations' do
-    it { should belong_to(:book) }
+    it { expect(subject).to belong_to(:book) }
   end
 end
--- spec/models/library_book_spec.rb
+++ spec/models/library_book_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe LibraryBook, :type => :model do
   context 'associations' do
-    it { should belong_to(:library) }
-    it { should belong_to(:book) }
+    it { expect(subject).to belong_to(:library) }
+    it { expect(subject).to belong_to(:book) }
   end
 end
