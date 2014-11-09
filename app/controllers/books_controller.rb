f0d14eb966a1bd7b376df8399e59ee3cf3fe4d1a
Roy Tay <toyray@users.noreply.github.com>
2014-11-09 12:27:12 +0000

    Save book if valid brn and borrowable and redirect to show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  def show
+  end
+
   def new
     @book = Book.new
   end
@@ -10,6 +13,10 @@ class BooksController < ApplicationController
       flash[:error] = 'No book with this BRN found.'
     elsif book.unavailable?
       flash[:error] = 'Book is not available for borrowing in any library.'
+    else
+      book.save
+      @book = book
+      redirect_to book_path(@book) and return
     end
     @book = Book.new
     render :new
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -9,31 +9,45 @@ RSpec.describe BooksController, :type => :controller do
   end
 
   describe "POST create" do
+    let(:params) { { brn: 1 } }
+
+    before { allow_any_instance_of(NLBService).to receive(:import_book).and_return(book) }
+
+    def do_request
+      post :create, book: params
+    end
+
     context 'when params are valid' do
-      pending
+      let(:book) { build(:book, :with_library_statuses) }
+
+      it 'saves book object' do
+        expect {
+          do_request
+        }.to change(Book, :count).by(1)
+      end
+
+      it 'redirects to show' do
+        do_request
+        expect(response).to redirect_to(action: :show, id: assigns(:book).id)
+      end
     end
 
     context 'when params are invalid' do
+      before { do_request }
+
       context 'when brn is invalid' do
-        let(:params) { { brn: 1 } } 
+        let(:book) { nil }
 
         it 'renders view with error' do
-          allow_any_instance_of(NLBService).to receive(:import_book).and_return(nil)
-
-          post :create, book: params
           expect(response).to render_template(:new)
           expect(flash[:error]).to_not be_nil
         end
       end
 
       context 'when book has no available libraries' do
-        let(:params) { { brn: 1 } }
-        let(:book)   { create(:book, :without_library_statuses) }
+        let(:book) { build(:book, :without_library_statuses) }
 
         it 'renders view with error' do
-          allow_any_instance_of(NLBService).to receive(:import_book).and_return(book)
-
-          post :create, book: params
           expect(response).to render_template(:new)
           expect(flash[:error]).to_not be_nil
         end
