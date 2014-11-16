a6f388507c30d72cabb087e8b8d267ad3544793e
Roy Tay <toyray@users.noreply.github.com>
2014-11-16 15:29:20 +0000

    Added basic view to show Book info after importing it

--- app/models/library_book.rb
+++ app/models/library_book.rb
@@ -1,4 +1,6 @@
 class LibraryBook < ActiveRecord::Base
   belongs_to :library
   belongs_to :book
+
+  delegate :name, to: :library, prefix: true
 end
--- spec/models/library_book_spec.rb
+++ spec/models/library_book_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe LibraryBook, :type => :model do
     it { is_expected.to belong_to(:library) }
     it { is_expected.to belong_to(:book) }
   end
+
+  context 'delegations' do
+    it { is_expected.to delegate_method(:library_name).to(:library).as(:name) }
+  end
 end
