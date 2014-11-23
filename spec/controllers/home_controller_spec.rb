bd8183ade1651bbc842527cf8d8c00c0f74ba6fa
Roy Tay <toyray@users.noreply.github.com>
2014-11-23 13:51:35 +0000

    Home controller now redirects to books#index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
   describe "GET index" do
-    it 'renders template' do
+    it 'redirects to books#index' do
       get :index
-      expect(response).to render_template :index
+      expect(response).to redirect_to :books
     end
   end
 end
