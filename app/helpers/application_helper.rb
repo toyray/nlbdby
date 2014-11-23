d2db2d077e4caa327e377cc7f0e7bd45de1619ff
Roy Tay <toyray@users.noreply.github.com>
2014-11-23 12:03:36 +0000

    Updated show book view to use Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def attr_row(object, attribute)
+    content_tag(:tr) do
+      concat content_tag(:td, object.class.human_attribute_name(attribute), class: 'col-xs-4')
+      concat content_tag(:td, object.try(attribute) || '', class: 'col-xs-8')
+    end
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, :type => :helper do
+  describe '#attr_row' do
+    let(:book) { build(:book, pages: 233) }
+
+    it { expect(helper.attr_row(book, :pages)).to eq('<tr><td class="col-xs-4">Pages</td><td class="col-xs-8">233</td></tr>') }
+  end
+end
