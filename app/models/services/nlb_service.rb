e22a44359c6828f9a22d0379cc7aaa611af312ab
Roy Tay <toyray@users.noreply.github.com>
2014-12-14 13:25:08 +0000

    Extracted method to read call no of book since updating book availability does not need to update call no

--- app/models/services/nlb_service.rb
+++ app/models/services/nlb_service.rb
@@ -10,6 +10,7 @@ class NLBService
     return nil if doc.title == 'No item found'
 
     extract_book_details(book, doc.css('table[summary="New Arrival Titles"] tr'))
+    extract_call_no(book, doc.css('table#ItemsTable tr'))
     extract_library_details(book, doc.css('table#ItemsTable tr'))
   end
 
@@ -26,6 +27,26 @@ class NLBService
     book.height = physical_info[/(\d+)\s*cm\./] || 0
 
     book
+  end
+
+  def extract_call_no(book, info)
+    return if book.call_no.present?
+
+    info.shift
+
+    info.each do |i|
+      library_info = i.css('td')
+
+      lending_type = library_info[2].content
+      call_info = library_info[3].content
+      
+      if !lending_type.start_with?('Accompanying')
+        book.call_no = call_info[/(?:(?:SING|LR)\s)?((?:\d+[\.\d]*\s)?(?:[A-Z]{2,3}))/, 1]
+        book.section = call_info[/\[([A-Z]+)\]/, 1]
+        return book
+      end
+    end 
+    book    
   end
 
   def extract_library_details(book, info)
@@ -46,11 +67,6 @@ class NLBService
           singapore: /SING/.match(call_info).present?,
           reference: lending_type == 'Lending Reference'
         }
-      end
-
-      if book.call_no.nil? && !lending_type.start_with?('Accompanying')
-        book.call_no = call_info[/(?:(?:SING|LR)\s)?((?:\d+[\.\d]*\s)?(?:[A-Z]{2,3}))/, 1]
-        book.section = call_info[/\[([A-Z]+)\]/, 1]
       end
     end 
     book
