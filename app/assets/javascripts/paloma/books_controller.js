851bce3f8db7da62aa4f3735aa9ca057a0422d56
Roy Tay <toyray@users.noreply.github.com>
2015-01-04 14:19:44 +0000

    Book can be rated at show view

--- app/assets/javascripts/paloma/books_controller.js
+++ app/assets/javascripts/paloma/books_controller.js
@@ -8,3 +8,13 @@ BooksController.prototype.index = function() {
     $.post('/books/' + id + '/rate.json', { rating: rating });
   });
 };
+
+BooksController.prototype.show = function() {
+  $(".responsive-rater").on("rated", function(e){
+    var id = $(this).data("rater-id"); 
+    var rating = $(this).data("rater-value");
+
+    $.post('/books/' + id + '/rate.json', { rating: rating });
+  });
+};
+
