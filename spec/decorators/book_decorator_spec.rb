d1dafbba6de35bfc08a5771ff3763c3d8c647715
Roy Tay <toyray@users.noreply.github.com>
2015-03-15 13:30:30 +0000

    Added specs for BookDecorator#availability_status

--- spec/decorators/book_decorator_spec.rb
+++ spec/decorators/book_decorator_spec.rb
@@ -1,4 +1,20 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe BookDecorator do
+RSpec.describe BookDecorator, :type => :decorator do
+  let(:object) { build_stubbed(:book) }
+  let(:subject) { object.decorate }
+
+  describe '#availability_status' do
+    context 'when book is available' do
+      before { allow(object).to receive(:available?).and_return(true) }
+
+      it { expect(subject.availability_status(nil)).to eq('YES') }
+    end
+
+    context 'when book is unavailable' do
+      before { allow(object).to receive(:available?).and_return(false) }
+
+      it { expect(subject.availability_status(nil)).to eq('NO') }
+    end
+  end
 end
