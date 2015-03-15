4280d80abd2c2a5f68352eb53a53aec3acdbe04d
Roy Tay <toyray@users.noreply.github.com>
2015-03-15 14:07:09 +0000

    Show reference badge for reference books

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -1,16 +1,15 @@
 class BookDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
   def availability_status(library_id)
     object.available?(library_id) ? 'YES' : 'NO'
+  end
+
+  def reference_status(library_id)
+    if object.reference?(library_id)
+      h.content_tag :span, class: 'label label-info' do
+        'REF'
+      end
+    end
   end
 end
--- spec/decorators/book_decorator_spec.rb
+++ spec/decorators/book_decorator_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe BookDecorator, :type => :decorator do
       it { expect(subject.availability_status(nil)).to eq('NO') }
     end
   end
+
+  describe '#reference_status' do
+    context 'when book is a reference book' do
+      before { allow(object).to receive(:reference?).and_return(true) }
+
+      it { expect(subject.reference_status(nil)).to include('REF') }
+    end
+
+    context 'when book is not a reference book' do
+      before { allow(object).to receive(:referncee?).and_return(false) }
+
+      it { expect(subject.reference_status(nil)).to be_nil }
+    end
+  end  
 end
