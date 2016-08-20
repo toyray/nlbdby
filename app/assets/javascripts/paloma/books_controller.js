9e76c1d1dfd9ce7cb4a192e34d6bd290ffd8d427
Roy Tay <toyray@users.noreply.github.com>
2016-08-20 18:51:56 +0000

    Removed debug message

--- app/assets/javascripts/paloma/books_controller.js
+++ app/assets/javascripts/paloma/books_controller.js
@@ -9,14 +9,13 @@ BooksController.prototype.index = function() {
   });
 
   $("#js-queue-all").on("click", function() {
-    alert("hello");
     $(".js-queue").click();
   });
 };
 
 BooksController.prototype.show = function() {
   $(".responsive-rater").on("rated", function(e){
-    var id = $(this).data("rater-id"); 
+    var id = $(this).data("rater-id");
     var rating = $(this).data("rater-value");
 
     $.post('/books/' + id + '/rate.json', { rating: rating });
